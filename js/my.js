8be1f5d2d94e9d278e06bdbe34d8279dc68d404e
Liam Cullen <LiamCullenCS@gmail.com>
2019-04-30 14:09:56 +0100

    dropdown#

--- js/my.js
+++
@@ -1,7 +0,0 @@
-	$(document).ready(function(){
-		$('#myselection').on('change', function(){
-			var demovalue = $(this).val(); 
-			$("div.myDiv").hide();
-			$("#show"+demovalue).show();
-		});
-	});
